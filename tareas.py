0ac75687d4b512e61e1d226537d6e61327b343fd
flavianoelia <flavianoelia79@gmail.com>
2024-07-30 20:49:33 -0300

    Forzar aplicación de cambios desde ramaFlavia

--- tareas.py
+++ tareas.py
@@ -166,13 +166,6 @@ class ListaEnlazada:
         with open(archivo, mode='r') as file:
             reader = csv.reader(file)
             for row in reader:
-                id, descripcion, prioridad, categoria, completada = int(row[0]), row[1], int(row[2]), row[3], row[4] == 'True'
-                if not self.tarea_existe(descripcion):
-                    tarea = Tarea(id, descripcion, prioridad, categoria)
-                    tarea.completada = completada
-                    self.agregar_tarea_existente(tarea)
-                else:
-                    print(f"Error: La tarea con la descripción '{descripcion}' ya existe en el archivo CSV.")  # Mensaje de error agregado
                 try:
                     id, descripcion, prioridad, fecha_vencimiento, categoria, completada = int(row[0]), row[1], int(row[2]), row[3], row[4], row[5] == 'True'
                     if not self.tarea_existe(descripcion):
